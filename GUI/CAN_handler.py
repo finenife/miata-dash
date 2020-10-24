c79c2424545802d11d064c703fdfeef0b839701f
finenife <haydenjallen96@gmail.com>
2020-10-23 22:57:17 -0500

    Arb_ID Dict
    Added Arb ID function look up

--- GUI/CAN_handler.py
+++ GUI/CAN_handler.py
@@ -2,6 +2,10 @@ import socket
 import pickle
 import select
 import random
+import can
+import math
+import struct
+
 from time import sleep
 
 HOST = 'localhost'  # Standard loopback interface address (localhost)
@@ -15,15 +19,20 @@ frames = {
     "bost": 0,
     "sped": 0
     }
+
+ID = {
+    '0x360': Arb360 #1 rpm [0,1] rpm, 0.1 kPa [2,3] bost
+    '0x372': Arb372 #0.1 volts [0,1] batt  
+    '0x3E0': Arb3E0 #0.1 Kelvin [0,1] cool
+    }
+
 data = ""
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    #print("Connected!")
     s.setblocking(0)
-    #s.sendall(b'Start')
+    bus = can.Bus(interface='socketcan', channel='can0')
     while True:
-        #print("read")
         ready = select.select([s],[],[],0)
         if ready[0]:
             data = s.recv(10)
@@ -32,6 +41,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.send(pickle.dumps(frames))
             #print("sent")
             data=""
+        for msg in bus:
+            if msg.arbitration_id in ID
         frames["fuel"]= random.randrange(0, 15)
         frames["cool"]= random.randrange(0, 15)
         frames["bat"]= random.randrange(0, 15)
